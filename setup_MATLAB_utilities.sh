784b9e8db9297f36d2697c5c52ad0727cbe74a0e
Benoit Pasquier <pasquieb@uci.edu>
2017-10-16 19:57:57 -0700

    erased comments at the end of MATLAB setup .sh file

--- setup_MATLAB_utilities.sh
+++ setup_MATLAB_utilities.sh
@@ -30,34 +30,7 @@ for (( i=1; i<${arraylength}+1; i++ )); do
     echo "($i) Pulling ${DIRS[$i-1]} from ${REMOTE_REPOS[$i-1]}"
     cd ${DIRS[$i-1]} && git pull && cd ..
   else
-    echo "($i) Cloning ${DIRS[$i-1]} from ${REMOTE_REPOS[$i-1]}" 
+    echo "($i) Cloning ${DIRS[$i-1]} from ${REMOTE_REPOS[$i-1]}"
     git clone ${REMOTE_REPOS[$i-1]}
   fi
 done
-
-# Remove everything (for testing purposes)
-#cd ~;
-#rm -rf $DIRECTORY
-#git pull origin master;
-#
-#function doIt() {
-#	rsync --exclude ".git/" \
-#		--exclude ".DS_Store" \
-#		--exclude ".osx" \
-#		--exclude "bootstrap.sh" \
-#		--exclude "README.md" \
-#		--exclude "LICENSE-MIT.txt" \
-#		-avh --no-perms . ~;
-#	source ~/.bash_profile;
-#}
-#
-#if [ "$1" == "--force" -o "$1" == "-f" ]; then
-#	doIt;
-#else
-#	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
-#	echo "";
-#	if [[ $REPLY =~ ^[Yy]$ ]]; then
-#		doIt;
-#	fi;
-#fi;
-#unset doIt;
