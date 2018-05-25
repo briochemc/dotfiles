05351019c7ada82de124b40f7d4f59bd436de2e9
Benoit Pasquier <pasquieb@uci.edu>
2018-05-25 10:32:12 -0700

    Added revise.jl

--- .juliarc.jl
+++ .juliarc.jl
@@ -3,3 +3,9 @@ push!(LOAD_PATH, ".")
 
 # Run local _init.jl if present
 isfile("_init.jl") && include(joinpath(pwd(), "_init.jl"))
+
+# Revise and run without reloading manually
+@schedule begin
+    sleep(0.1)
+    @eval using Revise
+end
