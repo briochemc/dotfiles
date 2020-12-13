4a55a5b3da9426242b0c2961901d19f7d1653c91
Benoit Pasquier <briochemc@gmail.com>
2020-12-14 10:41:17 +1100

    update startup.jl

--- startup.jl
+++ startup.jl
@@ -1,19 +1,24 @@
 ENV["GKS_ENCODING"] = "utf-8"
 
-try
-    using Revise
-catch e
-    @warn(e.msg)
+atreplinit() do repl
+    try
+        @eval using Revise
+        @async Revise.wait_steal_repl_backend()
+    catch e
+        @warn "Error initializing Revise" exception=(e, catch_backtrace())
+    end
+
+    try
+        @eval using OhMyREPL
+    catch e
+        @warn "error while importing OhMyREPL" e
+    end
+
+    try
+        @eval using ClearStacktrace
+    catch e
+        @warn e.msg
+    end
 end
 
-try
-    using OhMyREPL
-catch e
-    @warn(e.msg)
-end
 
-try
-    using ClearStacktrace
-catch e
-    @warn(e.msg)
-end
