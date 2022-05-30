7a5790ab90d46f660e8107b370576bef331da03a
Benoit Pasquier <briochemc@gmail.com>
2022-05-30 15:49:59 +1000

    Fix startup.jl

--- startup.jl
+++ startup.jl
@@ -2,23 +2,27 @@ ENV["GKS_ENCODING"] = "utf-8"
 
 atreplinit() do repl
     try
-        @eval using Revise
-        @async Revise.wait_steal_repl_backend()
-    catch e
-        @warn "Error initializing Revise" exception=(e, catch_backtrace())
-    end
-
-    try
         @eval using OhMyREPL
     catch e
         @warn "error while importing OhMyREPL" e
     end
-
-    try
-        @eval using ClearStacktrace
-    catch e
-        @warn e.msg
-    end
 end
 
+try
+    using Revise
+catch e
+    @warn "Error initializing Revise" exception=(e, catch_backtrace())
+end
+
+try
+    using ClearStacktrace
+catch e
+    @warn e.msg
+end
+
+try
+    using AbbreviatedStackTraces
+catch e
+    @warn e.msg
+end
 
